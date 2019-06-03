b48bc7cc5b47ff3d65650d47ec8f577d97090b89
Billy Joseph <ingbillyjoseph@gmail.com>
2019-06-03 18:58:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+  configure do
+    enable :sessions
+    set :session_secret, 'as;dfjaksl;dfjalsdfkjalsdfjlad'
+  end
+  
+  get '/' do
+    @session = session
+    
+    erb :index
+  end
+  
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
+  end
